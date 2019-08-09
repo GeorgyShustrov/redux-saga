b08a30c137aa76b3b889768087899c72fad1a1aa
GeorgyShustrov <gosha-s.1999@yandex.ru>
2019-08-09 16:18:51 +0300

    delete all old files, it's only empty app

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,8 @@
 import React from "react";
 import "./App.css";
-import { Route, Redirect, Switch } from "react-router-dom";
 import { logInStorage, logOutStorage } from "./store/duck";
 import { isLoginSelector } from "./store/selectors";
 import { connect } from "react-redux";
-import Login from "./containers/login";
-import AppRouter from "./components/AppRouter/AppRouter";
 
 const mapStateToProps = state => {
   return {
@@ -20,29 +17,9 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-const PrivateRoute = ({ component: Component, permited, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      permited ? <Component {...props} /> : <Redirect to="/login" />
-    }
-  />
-);
-
 class App extends React.Component {
   render() {
-    const { isLogin } = this.props;
-    return (
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <PrivateRoute
-          path="/dashboard"
-          permited={isLogin}
-          component={AppRouter}
-        />
-        <Redirect path="/*" to="/login" />
-      </Switch>
-    );
+    return <div>Тест</div>;
   }
 }
 
